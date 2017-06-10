7c931b8be33ac6e741e6cba310e74f47046afd8c
Daniil <daniil-rostov@list.ru>
2017-06-10 17:44:33 +0300

    dobavka-6

--- orders/views.py
+++ orders/views.py
@@ -10,7 +10,10 @@ def basket_adding(request):
     product_id = data.get("product_id")
     nmb = data.get("nmb")
 
-    new_product = ProductInBasket.objects.create(session_key=session_key, product_id=product_id, nmb=nmb)
+    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, defaults={"nmb": nmb})
+    if not created:
+        new_product.nmb += int(nmb)
+        new_product.save(force_update=True)
     product_total_nmb = ProductInBasket.objects.filter(session_key=session_key, is_active=True).count()
     return_dict["product_total_nmb"] = product_total_nmb
 
