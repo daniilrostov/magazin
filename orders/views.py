aa327b1a0e1f78c5bb76f60b62020a4713fdd84f
Daniil <daniil-rostov@list.ru>
2017-06-13 13:59:19 +0300

    dobavka-7

--- orders/views.py
+++ orders/views.py
@@ -14,7 +14,16 @@ def basket_adding(request):
     if not created:
         new_product.nmb += int(nmb)
         new_product.save(force_update=True)
-    product_total_nmb = ProductInBasket.objects.filter(session_key=session_key, is_active=True).count()
+    products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
+    product_total_nmb = products_in_basket.count()
     return_dict["product_total_nmb"] = product_total_nmb
 
+    return_dict["products"] = list()
+
+    for item in products_in_basket:
+        product_dict = dict()
+        product_dict["name"] = item.product.name
+        product_dict["price_per_item"] = item.price_per_item
+        product_dict["nmb"] = item.nmb
+        return_dict["products"].append(product_dict )
     return JsonResponse(return_dict)
