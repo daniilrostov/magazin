91f49ce08277f55459726ae685355f2143b7efc6
Daniil <daniil-rostov@list.ru>
2017-06-16 15:04:48 +0300

    dobavka-8

--- orders/views.py
+++ orders/views.py
@@ -9,11 +9,17 @@ def basket_adding(request):
     data = request.POST
     product_id = data.get("product_id")
     nmb = data.get("nmb")
+    is_delete = data.get("is_delete")
 
-    new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id, defaults={"nmb": nmb})
-    if not created:
-        new_product.nmb += int(nmb)
-        new_product.save(force_update=True)
+    if is_delete == 'true':
+        ProductInBasket.objects.filter(id=product_id).update(is_active=False)
+    else:
+        new_product, created = ProductInBasket.objects.get_or_create(session_key=session_key, product_id=product_id,
+                        is_active=True, defaults={"nmb": nmb})
+        if not created:
+            new_product.nmb += int(nmb)
+            new_product.save(force_update=True)
+
     products_in_basket = ProductInBasket.objects.filter(session_key=session_key, is_active=True)
     product_total_nmb = products_in_basket.count()
     return_dict["product_total_nmb"] = product_total_nmb
@@ -22,6 +28,7 @@ def basket_adding(request):
 
     for item in products_in_basket:
         product_dict = dict()
+        product_dict["id"] = item.id
         product_dict["name"] = item.product.name
         product_dict["price_per_item"] = item.price_per_item
         product_dict["nmb"] = item.nmb
